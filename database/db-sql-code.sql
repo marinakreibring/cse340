c8d5d06498201db5311424d997d650f670f1b9a7
marinakreibring <timarina1997@yahoo.com>
2025-11-08 12:54:47 +0100

    save changes to database

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -6,7 +6,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340mk;
+    OWNER TO cse340;
 
 --Table structure for table `classification`
 CREATE TABLE public.classification(
