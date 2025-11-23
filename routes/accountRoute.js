fcadc4d515b26a2de51b6be34ecdd8bf43571ad2
marinakreibring <timarina1997@yahoo.com>
2025-11-23 17:59:22 +0100

    polish registration view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,15 +1,30 @@
 // Needed Resources 
 const express = require("express");
 const router = new express.Router();
-const utilities = require("../utilities/index"); // the utilities > index file
+const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation')
 const { route } = require("./static");
+
 // week 4 - login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // week 4 - registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
-// week 4 - process registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// week 4 - Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 // export the router
 module.exports = router;
